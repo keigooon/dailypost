57163fef93624de083e981b4c8a950f3a80a09d7
keigo.yago <keigoya1995@gmail.com>
2020-12-01 11:01:26 +0000

    add fix before_action at users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:show]
-  before_action :not_logged_in, only: [:new, :create, :edit, :update]
+  before_action :require_user_logged_in, only: [:show, :edit, :update]
+  before_action :not_logged_in, only: [:new, :create]
 
  
   def show
